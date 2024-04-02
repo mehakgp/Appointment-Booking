f8e97734fc38cd6b71691bba49d1c4cef6ac4581
mehakgupta916 <mehak.g@mindfiresolutions.com>
2024-04-02 09:53:10 +0530

    improved css

--- Appointment.MVC/wwwroot/js/BookAppointment.js
+++ Appointment.MVC/wwwroot/js/BookAppointment.js
@@ -7,16 +7,39 @@
             url: "/Appointment/GetTimeSlots",
             type: "GET",
             data: { doctorId: doctorId, appointmentDate: appointmentDate },
-            success: function (data) {
-                var options = "<option value=''>Select Time</option>";
+            success: function (data)
+            {
+                var timeSlotsDiv = $("#timeSlots");
+                timeSlotsDiv.empty();
+
                 for (var i = 0; i < data.length; i++) {
-                    options += "<option value='" + data[i] + "'>" + data[i] + "</option>";
+                    var timeSlot = $("<div class='time-slot'></div>")
+                        .text(formatTime(data[i]))
+                        .attr("data-time", data[i])
+                        .click(function () {
+                            $(".time-slot").removeClass("selected");
+                            $(this).addClass("selected");
+                            $("#selectedTimeSlot").val($(this).attr("data-time"));
+                        });
+
+                    timeSlotsDiv.append(timeSlot);
                 }
-                $("#appointmentTime").html(options);
+                $("#appointmentTimeLabel").show();
             },
-            error: function () {
+            error: function ()
+            {
                 alert("Error fetching time slots.");
             }
         });
     });
 });
+function formatTime(time) {
+    var hours = parseInt(time.split(":")[0]);
+    var minutes = parseInt(time.split(":")[1]);
+
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+
+    return hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ' ' + ampm;
+}
